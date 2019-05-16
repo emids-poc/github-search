9179923580401136d91e3d4894abc73577951934
EmidsTechCommunity <emidstechcommunity@gmail.com>
2019-05-16 17:14:49 +0530

    Removed Token

--- src/app/search/token.config.ts
+++ src/app/search/token.config.ts
@@ -1,5 +1,5 @@
 export const config = {
     //Github personal access token should be updated here
-    token: '9e566985e03912a23f93131c972158d01fffebe7',
+    token: '',
     orgName: 'emids-poc'
 };
