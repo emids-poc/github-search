6dbf037b45ee45ab3115422a601b465035fc607d
EmidsTechCommunity <emidstechcommunity@gmail.com>
2019-06-24 15:03:00 +0530

    Added Pagination CSS Fix for the Grid

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -15,26 +15,34 @@ export class SearchComponent implements OnInit {
   searchText: string;
   searchResults: any;
   displayedColumns: string[] = ['name', 'full_name', 'description', 'author', 'language', 'html_url'];
+  paginator: any;
 
   constructor(private http: HttpClient) {
     this.searchText = '';
     this.searchResults = {
       items: []
     };
+    this.paginator = {};
   }
 
   ngOnInit() { 
     this.search();
   }
 
-  search() {
-    this.http.get('https://api.github.com/search/repositories?q=' + this.searchText + '+in:name,description+org:' + config.orgName, {
+  search(pageNumber = 1) {
+    // remove # from the search string if any, to avoid the global search
+    let reg = new RegExp('#')
+    let search = this.searchText.replace(reg, '');
+
+    this.http.get('https://api.github.com/search/repositories?q=' + search + '+in:name,description+org:' 
+    + config.orgName +'&per_page=5&page=' + pageNumber, {
       headers: {
         'Authorization': 'token ' + config.token,
         'Content-Type': 'application/json'
       }
     }).subscribe(
-      (response: any) => {            
+      (response: any) => {  
+        // set response data
         this.searchResults = response;       
         _.forEach(this.searchResults.items, (res: any, index) => {          
           if (_.includes(res.description, '||')) {
@@ -43,7 +51,14 @@ export class SearchComponent implements OnInit {
               this.searchResults.items[index].description = _.trimStart(splitDesc[1]);
               this.searchResults.items[index].language = _.trimStart(splitDesc[2]);
           }
-        });    
+        });
+        
+        // add paginator info
+        this.paginator.totalPage = Math.ceil(this.searchResults.total_count/5); // 5 is item per page
+        this.paginator.currentPage = pageNumber; 
+        this.paginator.pageLocation1 = pageNumber <= 1 ? 1 : pageNumber - 1;
+        this.paginator.pageLocation2 = pageNumber <= 1 ? 2 : pageNumber;
+        this.paginator.pageLocation3 = pageNumber <= 1 ? 3 : pageNumber + 1;
       },
       (error: any) => {
         console.log(error)
--- src/app/search/token.config.ts
+++ src/app/search/token.config.ts
@@ -1,5 +1,5 @@
 export const config = {
     //Github personal access token should be updated here
-    token: '',
+    token: 'c82d3c307523c7e403633a07d2190f5911c91d5b',
     orgName: 'emids-poc'
 };
